e989c339632df1c4d1d5a3f43302547983774e7f
chinawutao <462755350@qq.com>
2023-10-05 21:48:09 -0500

    init

---
+++ LabSolutions/Lab3/1a/elibrary/src/test/java/edu/miu/cs/cs489/lab3_1a/elibrary/ElibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs489.lab3_1a.elibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
