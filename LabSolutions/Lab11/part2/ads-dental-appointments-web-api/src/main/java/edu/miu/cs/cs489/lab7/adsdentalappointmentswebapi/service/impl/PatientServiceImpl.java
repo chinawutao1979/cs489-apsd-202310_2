c6be1e086e4efede4bfd0620492311e15b25f4f7
chinawutao <462755350@qq.com>
2023-10-25 10:51:51 -0500

    update

--- LabSolutions/Lab11/part2/ads-dental-appointments-web-api/src/main/java/edu/miu/cs/cs489/lab7/adsdentalappointmentswebapi/service/impl/PatientServiceImpl.java
+++ LabSolutions/Lab11/part2/ads-dental-appointments-web-api/src/main/java/edu/miu/cs/cs489/lab7/adsdentalappointmentswebapi/service/impl/PatientServiceImpl.java
@@ -5,19 +5,23 @@ import edu.miu.cs.cs489.lab7.adsdentalappointmentswebapi.dto.PatientResponse;
 import edu.miu.cs.cs489.lab7.adsdentalappointmentswebapi.model.Patient;
 import edu.miu.cs.cs489.lab7.adsdentalappointmentswebapi.repository.PatientRepository;
 import edu.miu.cs.cs489.lab7.adsdentalappointmentswebapi.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PatientServiceImpl implements PatientService {
+    @Autowired
     private PatientRepository patientRepository;
 
-    public PatientServiceImpl() {
-        this.patientRepository = patientRepository;
-    }
+
+//    public PatientServiceImpl() {
+//        this.patientRepository = patientRepository;
+//    }
 
     @Override
     public Patient registerNewPatient(Patient newPatient) {
@@ -47,7 +51,8 @@ public class PatientServiceImpl implements PatientService {
 
     @Override
     public Patient findPatientById(Integer patientId) {
-        return (Patient) patientRepository.findAllById(Collections.singleton(patientId));
+        Optional<Patient> optionalPatient = patientRepository.findById(patientId);
+        return optionalPatient.orElse(null);
     }
 
 }
--- LabSolutions/Lab11/part2/ads-dental-appointments-web-api/src/test/java/edu/miu/cs/cs489/lab7/adsdentalappointmentswebapi/service/impl/PatientServiceImplTest.java
+++ LabSolutions/Lab11/part2/ads-dental-appointments-web-api/src/test/java/edu/miu/cs/cs489/lab7/adsdentalappointmentswebapi/service/impl/PatientServiceImplTest.java
@@ -1,44 +1,42 @@
 package edu.miu.cs.cs489.lab7.adsdentalappointmentswebapi.service.impl;
 
 import edu.miu.cs.cs489.lab7.adsdentalappointmentswebapi.model.Patient;
+import edu.miu.cs.cs489.lab7.adsdentalappointmentswebapi.repository.PatientRepository;
 import edu.miu.cs.cs489.lab7.adsdentalappointmentswebapi.service.PatientService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
-class PatientServiceImplTest {
-    @Autowired
-    private TestEntityManager entityManager;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PatientServiceImplTest {
 
     @Autowired
     private PatientService patientService;
 
+    @Autowired
+    private PatientRepository patientRepository;
+
     @Test
     public void testFindPatientByIdExists() {
-        // Create and save a patient
-        Patient savedPatient = entityManager.persistAndFlush(new Patient(/* your patient details here */));
 
-        // Find the patient by ID
-        Patient foundPatient = patientService.findPatientById(savedPatient.getPatientId());
+        // When we look for this patient
+        Patient foundPatient = patientService.findPatientById(1);
 
-        // Assert the patient was found
-        assertThat(foundPatient).isNotNull();
-        assertThat(foundPatient.getPatientId()).isEqualTo(savedPatient.getPatientId());
-        // ... further assertions on other fields if needed
+        // Then we should get a non-null result
+        assertNotNull(foundPatient);
+        assertEquals(1, foundPatient.getPatientId().intValue());
     }
 
     @Test
-    public void testFindPatientByIdNotExists() {
-        // Try finding a patient by a non-existing ID
-        Patient foundPatient = patientService.findPatientById(-1);
+    public void testFindPatientByIdInvalid() {
+        // Assuming there's no patient with ID 12345 in the database
+        Patient foundPatient = patientService.findPatientById(12345);
 
-        // Assert the patient was not found
-        assertThat(foundPatient).isNull();
+        // Then the result should be null
+        assertNull(foundPatient);
     }
 }
